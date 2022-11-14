6a5e8e5932a0eb17c912b4c6f6067b5ddc07ccc1
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-14 11:52:24 -0800

    WIP: adding user interface to galactic age calculator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import Age from './../src/age.js';
+import Age from './../src/age.js';
+
+
+function handleSubmission() {
+  event.preventDefault();
+  document.querySelector('#mercury').innerText = null;
+  document.querySelector('#venus').innerText = null;
+  document.querySelector('#mars').innerText = null;
+  document.querySelector('#jupiter').innerText = null;
+  let years = document.getElementById("age").value;
+  let expectancy = document.getElementById("expectancy").value;
+  let age = new Age(years, expectancy);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("#age-form").addEventListener("submit", handleSubmission);
+});
