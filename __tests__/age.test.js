466ef78a74f0d9e0d4d7d034c8d9d794e52db86a
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-14 09:29:27 -0800

    shoul return how many years are left on Venus

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -6,30 +6,35 @@ describe('Age', () => {
     const age = new Age(25,81);
     expect(age.years).toEqual(25);
     expect(age.expectancy).toEqual(81);
-  })
+  });
 
   test('should return the years in Mercury time', () => {
     const age = new Age(25);
     expect(age.mercury()).toEqual(104);
-  })
+  });
 
   test('should return the years in Venus time', () => {
     const age = new Age(25);
     expect(age.venus()).toEqual(40);
-  })
+  });
 
   test('should return the years in Mars time', () => {
     const age = new Age(25);
     expect(age.mars()).toEqual(13);
-  })
+  });
 
   test('should return the years in Jupiter time', () => {
     const age = new Age(25);
     expect(age.jupiter()).toEqual(2);
-  })
+  });
 
   test('should return how many years are left on Mercury', () => {
     const age = new Age(25, 81);
     expect(age.mercuryEx()).toEqual(233)
-  })
+  });
+
+  test('should return how many years are left on Venus', () => {
+    const age = new Age(25, 81);
+    expect(age.venusEx()).toEqual(90);
+  });
 })
