c393350a286b7a10397eec78faac44c274ccdc3f
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-14 13:56:20 -0800

    added error handling when no values are entered by the user

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Age from './../src/age.js';
 
 function handleSubmission(event) {
   event.preventDefault();
+  document.getElementById('error-id').setAttribute("class", "error");
   document.querySelector('#mercury').innerText = null;
   document.querySelector('#venus').innerText = null;
   document.querySelector('#mars').innerText = null;
@@ -13,14 +14,6 @@ function handleSubmission(event) {
   let years = document.getElementById("age").value;
   let expectancy = document.getElementById("expectancy").value;
   let age = new Age(years, expectancy);
-  console.log(age.years);
-  console.log(age.expectancy);
-  // if (age.years === undefined || age.expectancy === undefined) {
-  //   document.querySelector('#mercury').innerText = null;
-  //   document.querySelector('#venus').innerText = null;
-  //   document.querySelector('#mars').innerText = null;
-  //   document.querySelector('#jupiter').innerText = null;
-  //   document.querySelector('#error-id').removeAttribute("class");
   if(years > expectancy) {
     document.querySelector('#mercury').innerText = `You are ${age.mercury()} years old on Mercury and you have lived ${age.mercuryEx()} years past your life expectancy.`;
 
@@ -29,7 +22,7 @@ function handleSubmission(event) {
     document.querySelector('#mars').innerText = `You are ${age.mars()} years old on Mars and you have lived ${age.marsEx()} years past your life expectancy.`;
 
     document.querySelector('#jupiter').innerText = `You are ${age.jupiter()} years old on Jupiter and you have lived ${age.jupiterEx()} years past your life expectancy.`;
-  }else {
+  }else if (years < expectancy){
     document.querySelector('#mercury').innerText = `You are ${age.mercury()} years old on Mercury and you have ${age.mercuryEx()} years left to explore.`;
 
     document.querySelector('#venus').innerText = `You are ${age.venus()} years old on Venus and you have ${age.venusEx()} years left to explore.`;
@@ -37,6 +30,12 @@ function handleSubmission(event) {
     document.querySelector('#mars').innerText = `You are ${age.mars()} years old on Mars and you have ${age.marsEx()} years left to explore.`;
 
     document.querySelector('#jupiter').innerText = `You are ${age.jupiter()} years old on Jupiter and you have ${age.jupiterEx()} years left to explore.`;
+  }else{
+    document.querySelector('#mercury').innerText = null;
+    document.querySelector('#venus').innerText = null;
+    document.querySelector('#mars').innerText = null;
+    document.querySelector('#jupiter').innerText = null;
+    document.getElementById('error-id').removeAttribute("class");
   }
 }
 
