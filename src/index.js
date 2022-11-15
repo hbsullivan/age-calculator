6918aeeb7cf50cc200274182f0494ef5ac8bf848
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-14 16:07:29 -0800

    reviewed code and README for submission

--- src/index.js
+++ src/index.js
@@ -15,17 +15,17 @@ function handleSubmission(event) {
   let expectancy = document.getElementById("expectancy").value;
   let age = new Age(years, expectancy);
   if(years > expectancy) {
-    document.querySelector('#mercury').innerText = `You are ${age.mercury()} years old on Mercury and you have lived ${age.mercuryEx()} years past your life expectancy.`;
+    document.querySelector('#mercury').innerText =`You are ${age.mercury()} years old on Mercury and you have lived ${age.mercuryEx()} years past your life expectancy.`;
 
-    document.querySelector('#venus').innerText = `You are ${age.venus()} years old on Venus and you have lived ${age.venusEx()} years past your life expectancy.`;
+    document.querySelector('#venus').innerText =`You are ${age.venus()} years old on Venus and you have lived ${age.venusEx()} years past your life expectancy.`;
 
-    document.querySelector('#mars').innerText = `You are ${age.mars()} years old on Mars and you have lived ${age.marsEx()} years past your life expectancy.`;
+    document.querySelector('#mars').innerText =`You are ${age.mars()} years old on Mars and you have lived ${age.marsEx()} years past your life expectancy.`;
 
-    document.querySelector('#jupiter').innerText = `You are ${age.jupiter()} years old on Jupiter and you have lived ${age.jupiterEx()} years past your life expectancy.`;
+    document.querySelector('#jupiter').innerText =`You are ${age.jupiter()} years old on Jupiter and you have lived ${age.jupiterEx()} years past your life expectancy.`;
   }else if (years < expectancy){
-    document.querySelector('#mercury').innerText = `You are ${age.mercury()} years old on Mercury and you have ${age.mercuryEx()} years left to explore.`;
+    document.querySelector('#mercury').innerText =`You are ${age.mercury()} years old on Mercury and you have ${age.mercuryEx()} years left to explore.`;
 
-    document.querySelector('#venus').innerText = `You are ${age.venus()} years old on Venus and you have ${age.venusEx()} years left to explore.`;
+    document.querySelector('#venus').innerText =`You are ${age.venus()} years old on Venus and you have ${age.venusEx()} years left to explore.`;
 
     document.querySelector('#mars').innerText = `You are ${age.mars()} years old on Mars and you have ${age.marsEx()} years left to explore.`;
 
